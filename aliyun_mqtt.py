f77e1b351b1e9eb21f55373f3cdecb4dceef1a3e
chechaojun <briant_ccj@163.com>
2018-11-20 17:21:52 +0800

    能够连接到云

--- aliyun_mqtt.py
+++ aliyun_mqtt.py
@@ -1,30 +1,132 @@
+# coding=utf-8
+# !/usr/bin/python3
+
+import datetime
+import time
 import hmac
 import hashlib
+import math
 
-#device_name = input('请输入device_name:')
-#product_key = input('请输入product_key:')
-#device_secret = input('请输入device_secret:')
+TEST = 0
 
-device_name = 'node1'
-product_key = 'a1HDTMmVX6c'
-device_secret = 'oIvNXW7XGxGbcysRvnSqqGzu6LqgSbgf'
+ProductKey = "a1HDTMmVX6c"
+ClientId = "12345"  # 自定义clientId
+DeviceName = "node1"
+DeviceSecret = "oIvNXW7XGxGbcysRvnSqqGzu6LqgSbgf"
 
-timestamp_srt = '789'
-domain = 'iot-as-mqtt.cn-shanghai.aliyuncs.com'
+# signmethod
+signmethod = "hmacsha1"
+# signmethod = "hmacmd5"
 
-host = product_key + '.' + domain
+# 当前时间毫秒值
+us = math.modf(time.time())[0]
+ms = int(round(us * 1000))
+timestamp = str(ms)
+
+data = "".join(("clientId", ClientId, "deviceName", DeviceName,
+                "productKey", ProductKey, "timestamp", timestamp
+                ))
+# print(round((time.time() * 1000)))
+print("data:", data)
+
+if "hmacsha1" == signmethod:
+    ret = hmac.new(bytes(DeviceSecret, encoding="utf-8"),
+                   bytes(data, encoding="utf-8"),
+                   hashlib.sha1).hexdigest()
+elif "hmacmd5" == signmethod:
+    ret = hmac.new(bytes(DeviceSecret, encoding="utf-8"),
+                   bytes(data, encoding="utf-8"),
+                   hashlib.md5).hexdigest()
+else:
+    raise ValueError
+
+sign = ret
+print("sign:", sign)
+
+# ======================================================
+
+strBroker = ProductKey + ".iot-as-mqtt.cn-shanghai.aliyuncs.com"
 port = 1883
-device_id = product_key + '.' + device_name
-client_id = device_id + '|securemode=3,signmethod=hmacmd5,timestamp=' + timestamp_srt + '|'
-username = device_name + '&' + product_key
 
-hash_str = 'clientId' + device_id + 'deviceName' + device_name + 'productKey' + product_key + 'timestamp' + timestamp_srt
+client_id = "".join((ClientId,
+                     "|securemode=3",
+                     ",signmethod=", signmethod,
+                     ",timestamp=", timestamp,
+                     "|"))
+username = "".join((DeviceName, "&", ProductKey))
+password = sign
 
-password = hmac.new(product_key.encode('utf-8'), hash_str.encode('utf-8'), hashlib.md5).hexdigest()
+print("="*30)
+print("client_id:", client_id)
+print("username:", username)
+print("password:", password)
+print("="*30)
 
-print('host:' + host)
-#print('port:' + port)
-print('device_id:' + device_id)
-print('client_id:' + client_id)
-print('username:' + username)
-print('password:' + password)
+def secret_test():
+    DeviceSecret = "secret"
+    data = "clientId12345deviceNamedeviceproductKeypktimestamp789"
+    ret = hmac.new(bytes(DeviceSecret, encoding="utf-8"),
+                   bytes(data, encoding="utf-8"),
+                   hashlib.sha1).hexdigest()
+    print("test:", ret)
+
+
+# ======================================================
+# MQTT Initialize.--------------------------------------
+
+try:
+    import paho.mqtt.client as mqtt
+except ImportError:
+    print("MQTT client not find. Please install as follow:")
+    print("git clone http://git.eclipse.org/gitroot/paho/org.eclipse.paho.mqtt.python.git")
+    print("cd org.eclipse.paho.mqtt.python")
+    print("sudo python setup.py install")
+
+
+# ======================================================
+def on_connect(mqttc, obj, rc):
+    print("OnConnetc, rc: " + str(rc))
+
+    mqttc.subscribe("test", 0)
+
+
+def on_publish(mqttc, obj, mid):
+    print("OnPublish, mid: " + str(mid))
+
+
+def on_subscribe(mqttc, obj, mid, granted_qos):
+    print("Subscribed: " + str(mid) + " " + str(granted_qos))
+
+
+def on_log(mqttc, obj, level, string):
+    print("Log:" + string)
+
+
+def on_message(mqttc, obj, msg):
+    curtime = datetime.datetime.now()
+    strcurtime = curtime.strftime("%Y-%m-%d %H:%M:%S")
+    print(strcurtime + ": " + msg.topic + " " + str(msg.qos) + " " + str(msg.payload))
+    on_exec(str(msg.payload))
+
+
+def on_exec(strcmd):
+    print("Exec:", strcmd)
+    strExec = strcmd
+
+
+# =====================================================
+if __name__ == '__main__':
+    if TEST:
+        secret_test()
+        exit(0)
+
+    mqttc = mqtt.Client(client_id)
+    mqttc.username_pw_set(username, password)
+    mqttc.on_message = on_message
+    mqttc.on_connect = on_connect
+    mqttc.on_publish = on_publish
+    mqttc.on_subscribe = on_subscribe
+    mqttc.on_log = on_log
+
+    mqttc.connect(strBroker, port, 120)
+    mqttc.loop_forever()
